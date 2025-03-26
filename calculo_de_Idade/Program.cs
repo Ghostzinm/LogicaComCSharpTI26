d9107dda6d9a1db8dc368ad81bf46dfbde79afd6
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-03-26 10:59:48 -0300

    calculo_de_Idade
    Primeiro programa em C#

---
+++ calculo_de_Idade/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculo_de_Idade
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {   
+            //declara variavel inteiras
+            int ano_nascimento, ano_atual, Idade;  
+
+            //console.WriteLine = escreva
+            Console.WriteLine("Digite o ano do seu nascimento: ");
+            ano_nascimento = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o ano atual: ");
+            ano_atual = int.Parse(Console.ReadLine());
+            Idade = ano_atual - ano_nascimento;
+            Console.WriteLine("Sua Idade é " + Idade + " anos");
+
+        }
+    }
+}
