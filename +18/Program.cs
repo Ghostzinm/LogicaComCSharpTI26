2273011c081ac01485687cbf8b057efbc688e946
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-03-26 11:47:29 -0300

    indetificador_maior_de_Idade
    sabe se voce é ou não maior de idade

---
+++ +18/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idd;
+
+            Console.WriteLine("Digite sua idade: ");
+            idd = int.Parse(Console.ReadLine());
+            if (idd >= 18)
+            {
+                Console.WriteLine("voce é maior de idade");
+            } else {
+                Console.WriteLine("voce é menor de idade");
+            }
+        }
+    }
+}
