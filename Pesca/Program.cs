817646add09a355fe30ab94bda2fd1b7d7055a60
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-03-26 11:48:36 -0300

    pesca
    pesca e pesca

---
+++ Pesca/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pesca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double peso, sobra;
+
+            Console.WriteLine("Digite o peso pescado: ");
+            peso = double.Parse(Console.ReadLine());
+            sobra = (peso - 50) * 4;
+            Console.WriteLine(sobra);
+        }
+    }
+}
