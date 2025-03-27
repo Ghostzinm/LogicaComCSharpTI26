b10120e20f564bd4928c4a29dc092488b2c859da
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-03-27 09:12:12 -0300

    numero entre 20 e 90

---
+++ Numero entre 20 e 90/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numero_entre_20_e_90
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Contrua um algoritimo que indique se um numero esta entre 20 e 90
+            int numero;
+
+            Console.WriteLine("digite um numero: ");
+            numero = int.Parse(Console.ReadLine());
+            if(numero > 20 && numero < 90){
+                Console.WriteLine("Numero esta entre 20 e 90");
+             } else {
+                 Console.WriteLine("Numero nao esta entre 20 e 90");
+             }                   
+        }
+    }
+}
