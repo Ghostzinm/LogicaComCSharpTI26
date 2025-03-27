6a97e8b86c1fcf54e3d606161cc8c349f7e00309
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-03-27 11:08:35 -0300

    saber se é multiplo de 5 ou de 3

---
+++ multiplo de 5 ou de 3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplo_de_5_ou_de_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double num1, num3, num5;
+
+            Console.WriteLine("digite o numero ");
+            num1 = double.Parse(Console.ReadLine());
+
+
+
+            if(num1 % 5 == 0){
+                Console.WriteLine("numero é multiplo de 5 ");
+    }
+            if(num1 % 3 == 0){
+                Console.WriteLine("numero é multiplo de 3 ");
+    }
+            else {
+                Console.WriteLine("ne nao ");
+            }
+
+
+        }
+    }
+}
