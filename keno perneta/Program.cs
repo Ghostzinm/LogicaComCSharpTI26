64cc06d349d7c08b5323b1757e502ab2e15bf3bd
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-03-27 10:47:39 -0300

    keno perneta

---
+++ keno perneta/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace keno_perneta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pd, pe;
+
+            Console.WriteLine("keno, digite o tamanho da sua perna direita: ");
+            pd = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("keno, digite o tamanho da sua perna esquerda: ");
+            pe = Convert.ToDouble(Console.ReadLine());
+
+            if (pe > pd) {
+                Console.WriteLine("ala o cara é cotoco da perna direita kakakakak lol "); 
+            };
+
+            if(pd > pe){
+                Console.WriteLine("ala o cara é cotoco da perna esquerda kakakakak lol");
+            } else { Console.WriteLine("normal");
+            };
+
+        }
+    }
+}
