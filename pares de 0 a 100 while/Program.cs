50bef6d32364c973b76d990ca6ea40cdb72a14e6
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-03-27 09:37:18 -0300

    pares de 0 a 100 (while)
    pare de 0 a 100 usando o laço while

---
+++ pares de 0 a 100 while/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pares_de_0_a_100_while
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int contador = 0;
+
+            while (contador < 100)  {
+                contador = contador + 2;
+                Console.WriteLine(contador);
+            }
+
+        }
+    }
+}
