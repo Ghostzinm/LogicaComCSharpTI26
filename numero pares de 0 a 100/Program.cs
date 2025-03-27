4c762ca9e47fd66a621db24b9d0cfe0270a63980
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-03-27 09:24:09 -0300

    numero pares de 0 a 100

---
+++ numero pares de 0 a 100/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numero_pares_de_0_a_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////mostre numeros PARES de 0 a 100 (para)
+
+            int contador;
+              
+            for(contador = 0; contador < 101; contador = contador + 2){
+                Console.WriteLine(" " + contador);
+            }
+         }
+    }
+
+}
