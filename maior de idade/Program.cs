366d77df3ccd3fe08f82cfb2ea3747e12c2659be
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-03-27 11:26:19 -0300

    maior idade

---
+++ maior de idade/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maior_de_idade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double idade;
+
+
+            Console.WriteLine("insira sua idade ");
+            idade = double.Parse(Console.ReadLine());
+
+            if (idade > 17) {
+                Console.WriteLine("vc tem maior idade penal");
+            } else { Console.WriteLine("vc tem menor de idade penal"); }
+
+
+        }
+    }
+}
