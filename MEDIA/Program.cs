f2eee0f5b8052dd063270b5cf2868591224bb4a6
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-03-27 11:39:31 -0300

    media

---
+++ MEDIA/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MEDIA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota1, nota2, nota3, nota4, nota_total;
+
+            Console.WriteLine("nota de ciencioas: ");
+            nota1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("nota de portugues: ");
+            nota2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("nota de geografia: ");
+            nota3 = double.Parse(Console.ReadLine());
+            Console.WriteLine("nota de matematica: ");
+            nota4 = double.Parse(Console.ReadLine());
+            nota_total = (nota1 + nota2 + nota3 + nota4) / 4;
+            Console.WriteLine(" nota final " + nota_total);
+            if (nota_total >= 7)
+                Console.WriteLine(" aprovado ");
+            else {
+                Console.WriteLine(" reprovado");
+            }
+
+        }
+    }
+}
---
+++ MEDIA/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("MEDIA")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MEDIA")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("0b6b270a-0398-4522-84da-7a83d3291691")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
