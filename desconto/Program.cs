f82cad7f37fde71bb68bba7984093e33d2198932
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-03-27 11:14:48 -0300

    desconto de 10% acimia de 100 e 5% abaixo de 100

---
+++ desconto/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desconto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double num1, des, total;
+            Console.WriteLine("digite o numero ");
+            num1 = double.Parse(Console.ReadLine());
+
+            if (num1 >= 100)
+            {
+                des = num1 * 0.10;
+                total = num1 - des;
+                Console.WriteLine(des);
+            }
+            else
+            {
+                des = num1 * 0.05;
+                total = num1 - des;
+                Console.WriteLine(des);
+
+            }
+        }
+    }
+}
