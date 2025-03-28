92d1ad9810f91ef0de84d8cf85df4bdd9011fad6
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-03-28 08:50:37 -0300

    numero impares ate 100  (while)
    Mostre somente os números pares de 1 a 100.

---
+++ todos o numero impares ate 100(for)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todos_o_numero_impares_ate_100_while_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int impar = 1;
+
+            while (impar < 100)
+            {
+                Console.WriteLine("Números impar: " + impar);
+                impar = impar + 4 / 2;
+            }
+        }
+    }
+}
