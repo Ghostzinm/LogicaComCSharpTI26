4fc7358bc7d56e16fe34bbefb7abab614d92326b
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-03-28 09:09:15 -0300

    empretimo

---
+++ emprestimo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace emprestimo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double salario, prestacao;
+
+            Console.WriteLine("digite o seu salario ");
+            salario = double.Parse(Console.ReadLine());
+            Console.WriteLine("valor da prestação ");
+            prestacao = double.Parse(Console.ReadLine());
+            if (prestacao > salario * 30 / 100)
+                Console.WriteLine("emprestimo negado ");
+            else { Console.WriteLine("saldo aceito"); }
+        }
+    }
+}
