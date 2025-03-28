9714fcaa6d06ec099be4baf540b24a183cf3ab22
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-03-28 10:59:13 -0300

    A e B
    Faça um algoritmo que leia dois valores inteiros A e B, se os valores de A e B
    forem iguais, deverá somar os dois valores,
    caso contrário devera multiplicar A por B. Ao final de qualquer um dos cálculos
    deve-se atribuir o resultado a uma variável C e
    imprimir seu valor na tel

---
+++ A e B/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_e_B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Faça um algoritmo que leia dois valores inteiros A e B, se os valores de A e B 
+forem iguais, deverá somar os dois valores,
+caso contrário devera multiplicar A por B. Ao final de qualquer um dos cálculos 
+deve-se atribuir o resultado a uma variável C e
+imprimir seu valor na tela*/
+
+            int a, b, total;
+
+            Console.WriteLine("digite um numero");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("digite mais um numero");
+            b = int.Parse(Console.ReadLine());
+
+            if (a == b)
+            {
+                total = a + b;
+                Console.WriteLine(total);
+
+            }
+            else
+            {
+                total = a * b;
+                Console.WriteLine(total);
+            }
+        }
+    }
+}
