d9f4b7e9c6dc12f367bf67bf78ae0ec6ee3c5d6c
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-03-28 11:21:38 -0300

    reajuste de 5%
    Faça um algoritmo que leia um valor qualquer e imprima na tela com um
    reajuste de 5%.

---
+++ reajuste de 5%/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reajuste_de_5_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Faça um algoritmo que leia um valor qualquer e imprima na tela com um 
+reajuste de 5%.*/
+
+            double valor, ajuste, sobra;
+
+            Console.WriteLine("digite um valor");
+            valor = double.Parse(Console.ReadLine());
+
+            sobra = valor * 0.05;
+            ajuste = valor + sobra;
+            Console.WriteLine(ajuste);
+
+        }
+    }
+}
