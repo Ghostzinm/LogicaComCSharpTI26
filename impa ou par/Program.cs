c878460ddf7dc0d1e0e6e6a90bcd804918534986
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-03-28 09:14:18 -0300

    impa ou par

---
+++ impa ou par/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace impa_ou_par
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1;
+            Console.WriteLine("Digite um numero: ");
+            num1 = double.Parse(Console.ReadLine());
+            if(num1 % 2 == 1){
+                Console.WriteLine("Este numero é impar");
+    }
+            else{
+                Console.WriteLine("Este numero é par");
+    }
+        }
+    }
+}
