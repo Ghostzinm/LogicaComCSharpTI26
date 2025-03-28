9e3252f6bd01525bbee87a86e5768f8aa72a6116
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-03-28 08:54:06 -0300

    todos os numero impares ate 100 (for)

---
+++ todos numeros impares ate 100 (for)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todos_numeros_impares_ate_100__for_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int impar;
+
+            for (impar = 1; impar < 100; impar++)
+            {
+                impar = impar + 4 / 2;
+                Console.WriteLine("Números impars: " + impar);
+
+            }
+        }
+    }
+}
