4d0fcaf14567212e33561c7ddf3f94e1971ba227
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-04-02 09:27:40 -0300

    troncando A e B
    7-  Faça um algoritmo que receba um valor A e B, e troque o valor de A por B e o valor de B por A e imprima na tela os valores.

---
+++ trocando valor de A e B/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trocando_valor_de_A_e_B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*7-  Faça um algoritmo que receba um valor A e B, e troque o valor de A por B e o valor de B por A e imprima na tela os valores.*/
+
+            double A, B;
+
+            Console.WriteLine("digite um valor: ");
+            A = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("digite um valor: ");
+            B = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("invertendo valor....  ");
+
+            Console.WriteLine("valoer de A é " + B + " e o valor de B " + A);
+        }
+    }
+}
