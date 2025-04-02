1fb6a4c6ddfc6fcb5b12034b27a3e91c59f3c863
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-04-02 09:55:31 -0300

    fahrenheit to celsius
    9- Faça um algoritmo que leia uma temperatura em Fahrenheit e calcule a temperatura correspondente em grau Celsius. Imprima na tela as duas temperaturas.
    Fórmula: C = (5 * ( F-32) / 9)

---
+++ celsos para Fahrenheit/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace celsos_para_Fahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*9- Faça um algoritmo que leia uma temperatura em Fahrenheit e calcule a temperatura correspondente em grau Celsius. Imprima na tela as duas temperaturas.
+Fórmula: C = (5 * ( F-32) / 9)
+*/
+
+            double F, C, cauculo;
+
+            Console.WriteLine("dgite temperatura em Fahrenheit");
+            F = double.Parse(Console.ReadLine());
+
+            C = 5 * (F - 32) / 9;
+
+            Console.WriteLine(F + "º em Fahrenheits é " + C + "º em Celsius");
+
+
+        }
+    }
+}
---
+++ contatodor de idade/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contatodor_de_idade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
